b3a4f7c95c1c232f0ad5142405a956d0be9973f3
Hakan <hakan.bahcivanci@gmail.com>
2020-12-15 23:12:24 +0100

    Added 3d models, added MissileLauncher controlpanel

---
+++ Assets/Scripts/Buildings/MissileLauncherMouseSelection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class MissileLauncherMouseSelection : MouseSelectionScript
+{
+    void Start()
+    {
+        Debug.Log("S T A R T");
+        TargetAction = shootMissile;
+    }
+
+
+    private void shootMissile(GameObject target)
+    {
+        Debug.Log("Shoot missile to:" + target.transform.position);
+        GetComponent<MissileLauncherController>().fireMissile(target);
+    }
+}
